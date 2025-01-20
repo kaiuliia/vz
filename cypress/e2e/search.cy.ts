e429c5d0c15f525368c4be124c6d491004ce865b
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2025-01-20 22:27:10 +0100

    Add e2e test.

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,30 @@
+describe('search', () => {
+  it('should have form', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input').should('have.value', '');
+    cy.get('select').should('have.value', 'movie');
+    cy.get('button').should('have.text', 'Submit');
+  });
+
+  it('should display required error when the title is empty', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('button[type="submit"]').click();
+    cy.get('[role="alert"]').should('contain', 'Please, enter a movie title');
+  });
+
+  it('should display search results', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input[placeholder="movie title"]').type('Harry Potter');
+    cy.get('select').select('movie');
+    cy.get('input[placeholder="year"]').type('2004');
+    cy.get('button[type="submit"]').click();
+    cy.get('ul li').should('have.length.greaterThan', 0);
+    cy.get('ul li')
+      .first()
+      .within(() => {
+        cy.get('h3').should('exist').and('not.be.empty');
+        cy.get('p').should('exist').and('not.be.empty');
+        cy.get('img').should('have.attr', 'src').and('not.be.empty');
+      });
+  });
+});
